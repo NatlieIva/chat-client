14a6a2baddb869dc062d7d7300ae773a543d6ac8
natalieivanova <natalie_iva@icloud.com>
2021-01-13 11:28:55 +0300

    added model and service logic

--- src/main/java/ru/itsjava/model/User.java
+++ src/main/java/ru/itsjava/model/User.java
@@ -2,29 +2,8 @@ package ru.itsjava.model;
 
 import lombok.Data;
 
-//@Data
+@Data
 public class User {
     private String name;
     private String password;
-
-    public User(String name, String password) {
-        this.name = name;
-        this.password = password;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 }
